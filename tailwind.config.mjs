8f68e3812d1a4046d111e50e1c2e7711e036354f
Daniel May <danielmamay@gmail.com>
2019-12-13 20:56:46 +0000

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+import typography from '@tailwindcss/typography';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography],
+};
